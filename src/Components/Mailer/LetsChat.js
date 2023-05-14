3ccced540c3b1126ed48865fdeae42581c997da4
ARS Talukder <talukderars@gmail.com>
2023-05-15 02:39:00 +0600

    all done

--- src/Components/Mailer/LetsChat.js
+++ src/Components/Mailer/LetsChat.js
@@ -7,7 +7,7 @@ const LetsChat = () => {
     return (
         <div className='mt-20 mb-1 flex justify-end'>
             <div>
-                <ReactWhatsapp className='border border-4 border-green-400 rounded-3xl hover:border-green-500 mb-4' number="+880 1629 396785" message="Hello, MD. ABDUR RAHMAN. I am interested to your work">
+                <ReactWhatsapp className='border border-4 border-green-400 rounded-3xl hover:border-green-500 mb-4' number="+880 1629 396785" message="Hello, MD. ABDUR RAHMAN. I want to get in touch with you.">
                     <img src="https://i.ibb.co/p67tz8w/Chat.png" width={100} alt="chat" />
                 </ReactWhatsapp>
                 <div className='flex justify-end'>
